9438b5dc74467b742bdf1dcda7517e6fb3afa2fe
Williams Ccompi <ccompiguerrerow@gmail.com>
2024-10-04 21:20:41 -0500

    Evento Actualizado

--- EventoAPI/Program.cs
+++ EventoAPI/Program.cs
@@ -1,4 +1,6 @@
+using Evento.DOMAIN.Core.interfaces;
 using Evento.DOMAIN.Infraestructure.Data;
+using Evento.DOMAIN.Infraestructure.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +14,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IAttendeesRepository, AttendeesRepository>();
 
 var app = builder.Build();
 
